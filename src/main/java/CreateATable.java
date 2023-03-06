7e1ec3bbd3e4b8869b05c64cbb4c028568a8d895
sushmishra87 <sm.sushmimishra@gmail.com>
2023-03-06 17:41:42 +0000

    Completed Lab Create Table

--- src/main/java/CreateATable.java
+++ src/main/java/CreateATable.java
@@ -65,6 +65,7 @@ public class CreateATable {
      *     Do not change anything in this code. You should write your sql statement on a single line in the
      *     problem1.sql file.
      */
+
     public void problem1(){
 //        write your SQL logic in a single line in the problem1.sql file.
         String sql = FileUtil.parseSQLFile("problem1.sql");
